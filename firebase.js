0de2a3b64a021d2e2b86b3ab8f0bd6cd729f1c30
rafaelsantos458 <rafaelsantos458@gmail.com>
2026-04-25 18:09:44 -0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB1YFgKEM7_0fj3WGeXwDpNpNtqNY8URCM",
-  authDomain: "meu-hotel-online-ac0ea.firebaseapp.com",
-  projectId: "meu-hotel-online-ac0ea",
-  storageBucket: "meu-hotel-online-ac0ea.firebasestorage.app",
-  messagingSenderId: "890702641890",
-  appId: "1:890702641890:web:058da71584471b32297cbb"
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI",
+  projectId: "COLE_AQUI",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
